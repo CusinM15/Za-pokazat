822f7487a7980df5d7a58556fb476eead9251758
Matej Čušin <44202906+CusinM15@users.noreply.github.com>
2022-08-30 19:35:31 +0200

    Delete caranje-vaje directory

--- caranje-vaje/simerticna.m
+++
@@ -1,5 +0,0 @@
-function mat = simerticna(n)
-    %sprejme n vrne pa simetrično matriko n*n
-    M = tril(randi([0 9], n),-1);
-    mat = M + M' + eye(n);
-end
--- caranje-vaje/vaje1.m
+++
@@ -1,7 +0,0 @@
-x = 0:.01:1;
-y = 0:.01:2;
-f= @(x,y)sin(x.^2-y.^4) ./ (x.^2-y.^4);
-[x2d, y2d] = ndgrid(x, y);
-z = f(x2d, y2d);
-z(abs(sin(x2d.^2-y2d.^4) ./ (x2d.^2-y2d.^4)) < 10^-6)=1;
-surf(x2d, y2d, z)
